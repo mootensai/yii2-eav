5b7b00bae4569249f6fdeebe4eeea4d8d8c3dc0d
Dmitriy Derepko <xepozz@list.ru>
2019-12-18 22:35:39 +0300

    Fixed

--- src/migrations/m160711_162535_change_default_value.php
+++ src/migrations/m160711_162535_change_default_value.php
@@ -6,12 +6,11 @@ class m160711_162535_change_default_value extends Migration
 {
     public function safeUp()
     {
-        $this->alterColumn('{{%eav_attribute}}','description',$this->string(255)->defaultValue(''));
+        $this->alterColumn('{{%eav_attribute}}', 'description', $this->string(255)->defaultValue(''));
     }
 
     public function safeDown()
     {
-       $this->alterColumn('{{%eav_attribute}}','description',$this->string(255)->defaultValue('NULL'));
-        return false;
+        $this->alterColumn('{{%eav_attribute}}', 'description', $this->string(255)->defaultValue('NULL'));
     }
 }
